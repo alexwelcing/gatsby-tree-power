8805399008665a9e61da91add0fe88dbed270c58
Alex Welcing <alexwelcing@gmail.com>
2019-01-28 10:29:13 -0500

    Moved to next line.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -87,8 +87,8 @@ export class Layout extends React.PureComponent<{}> {
               <GlobalStyle />
               {children}
               <Footer>
-                &copy; {split(data.site.buildTime, '.')[2]} by Majid Hajian. All rights reserved. <br />
-                <a href="https://github.com/mhadaily/gatsby-starter-typescirpt-power-blog">GitHub Repository</a> <br />
+                &copy; {split(data.site.buildTime, '.')[2]} by Alex Welcing. All rights reserved. <br />
+                <a href="https://github.com/mhadaily/gatsby-starter-typescirpt-power-blog">Theme Repo by Majid Hajian</a> <br />
                 <span>Last build: {data.site.buildTime}</span>
               </Footer>
             </React.Fragment>
